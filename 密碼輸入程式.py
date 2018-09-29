dadbaed78b941b62f104e941f11f5e40f524ea45
Jack Yang <ja199102@gmail.com>
2018-09-29 16:33:35 +0800

    Final commit

--- 密碼輸入程式.py
+++ 密碼輸入程式.py
@@ -1,13 +1,14 @@
 password = 'a123456'
-x = 0
 i = 3 # 剩餘機會
-while True:
+while i > 0:
+	i = i - 1
 	pwd = input('請輸入密碼: ')
 	if pwd == password:
 		print('登入成功')
 		break
-	else:
-		i = i - 1	
-		print('密碼錯誤! 還有', i, '次機會')
-		if i == 0:
-			break
+	else:	
+		print('密碼錯誤!')
+		if i > 0:
+			print('還有', i, '次機會')
+		else:
+			print('超過輸入上限，鎖定帳號')
